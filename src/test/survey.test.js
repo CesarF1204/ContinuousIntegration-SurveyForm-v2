96c092bcdbde8cf5b531111c31fdad1e71e150c5
Cesar Francisco <princexcesar@gmail.com>
2021-06-10 07:24:48 +0800

    success test

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -21,11 +21,4 @@ describe("Survey Model", function(){
 
         expect(result).to.equal("Error! Captcha input doesn't matched.");
     });
-
-    it('Should return error message when input captcha does not matched.', function(){
-        let surveyModel = new SurveyModel();
-        let result = surveyModel.verifyCaptchaInput("random");
-
-        expect(result).to.equal("Failed Test Intentionally.");
-    });
 });
